8c28dd2ce429bbb4596859236b768e012861ba48
Raluca <raluca.constanda@gmail.com>
2026-04-09 12:04:12 +0200

    Fix cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,11 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Enable CORS for all routes
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors({
+  origin: function (origin, callback) {
+    callback(null, origin);
+  },
+  credentials: true
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
